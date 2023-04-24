a4b09f971240a78d46ab2574fc32b29ae0563cdc
nurcancurebal <nurcancurebal@hotmail.com>
2023-04-24 22:13:08 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>
-  <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
-
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
-</div>
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="4">
+          <AddRoleModel />
+        </b-col>
+        <b-col sm="8"></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddRoleModel from "./components/AddRoleModel";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AddRoleModel,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/AddRoleModel.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="addRoleModel mt-3">
+    <b-img :src="imageSource" v-bind="mainProps" rounded></b-img>
+    <b-form>
+      <b-form-input
+        class="mt-2"
+        v-model="modelName"
+        placeholder="What is your role model name"
+      ></b-form-input>
+      <b-form-input
+        class="mt-2"
+        v-model="modelJob"
+        placeholder="What is his job"
+      ></b-form-input>
+      <b-form-input
+        class="mt-2"
+        v-model="modelNationality"
+        placeholder="What is his nationality"
+      ></b-form-input>
+      <b-form-input
+        class="mt-2"
+        v-model="modelPic"
+        placeholder="Add his picture link"
+      ></b-form-input>
+
+      <label class="mt-3">Choose his birthday</label>
+      <b-form-datepicker
+        v-model="modelBirthday"
+        class="mb-2"
+      ></b-form-datepicker>
+
+      <b-form-textarea
+        class="mt-2"
+        v-model="modelAbout"
+        placeholder="Note about him..."
+        rows="3"
+        max-rows="6"
+      ></b-form-textarea>
+
+      <b-form-tags
+        input-id="tags-basic"
+        v-model="tags"
+        class="mt-2"
+      ></b-form-tags>
+
+      <b-button @click="sendRoleModel()" block variant="outline-primary"
+        >Primary</b-button
+      >
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      modelName: "",
+      modelJob: "",
+      modelNationality: "",
+      modelBirthday: "",
+      modelAbout: "",
+      modelPic: "",
+      tags: [],
+      imageSource:
+        "https://us.123rf.com/450wm/kakigori/kakigori1808/kakigori180800024/107433935-cute-funny-man-dad-and-kid-son-wearing-black-mustache-mask.jpg?ver=6",
+      mainProps: {
+        width: 150,
+        height: 150,
+        class: "m1",
+      },
+    };
+  },
+
+  methods: {
+    sendRoleModel() {
+      console.log(this.modelName);
+      console.log(this.modelJob);
+      console.log(this.modelNationality);
+      console.log(this.modelBirthday);
+      console.log(this.modelAbout);
+      console.log(this.tags);
+    },
+  },
+};
+</script>
+
+
+<style>
+.addRoleModel {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
